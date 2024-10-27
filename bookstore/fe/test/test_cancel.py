b09b59889bd7aa29aafcccafe87ee1b914aaf400
Avalonere <144768885+Avalonere@users.noreply.github.com>
2024-10-27 20:55:49 +0800

    更正取消订单的测试

--- bookstore/fe/test/test_cancel.py
+++ bookstore/fe/test/test_cancel.py
@@ -3,26 +3,20 @@
 import pytest
 
 from fe.access.new_buyer import register_new_buyer
-from fe.access.new_seller import register_new_seller
-from fe.access.book import Book
 from fe.test.gen_book_data import GenBook
 import uuid
 
 
 class TestCancel:
-    buy_book_info_list: [Book]
-
     @pytest.fixture(autouse=True)
     def pre_run_initialization(self):
-        self.seller_id = "test_cancel_seller_id_{}".format(str(uuid.uuid1()))
-        self.store_id = "test_cancel_store_id_{}".format(str(uuid.uuid1()))
-        self.buyer_id = "test_cancel_buyer_id_{}".format(str(uuid.uuid1()))
+        self.seller_id = "test_new_order_seller_id_{}".format(str(uuid.uuid1()))
+        self.store_id = "test_new_order_store_id_{}".format(str(uuid.uuid1()))
+        self.buyer_id = "test_new_order_buyer_id_{}".format(str(uuid.uuid1()))
         self.password = self.seller_id
         self.buyer = register_new_buyer(self.buyer_id, self.password)
-        self.seller = register_new_seller(self.seller_id, self.password)
         self.gen_book = GenBook(self.seller_id, self.store_id)
         yield
-
 
     def test_cancel_order_and_repeated_cancel(self):
         ok, buy_book_id_list = self.gen_book.gen(
@@ -85,7 +79,7 @@ class TestCancel:
         code, order_id = self.buyer.new_order(self.store_id, buy_book_id_list)
         assert code == 200
 
-        code = self.buyer.add_funds("999999999")
+        code = self.buyer.add_funds(99999999)
         assert code == 200
 
         code = self.buyer.payment(order_id)
